56d843dae01ab5569677f2c4614c8d744bbabf9a
Netto Farah <nettofarah@gmail.com>
2020-09-08 11:03:21 -0700

    Moar tests

--- src/components/Typography/__tests__/Heading.test.tsx
+++ src/components/Typography/__tests__/Heading.test.tsx
@@ -1,11 +1,33 @@
 import React from 'react'
-import * as ReactDOM from 'react-dom'
 import { Heading } from '../Heading'
+import testRenderer from 'react-test-renderer'
 
 describe('Heading', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div')
-    ReactDOM.render(<Heading size="heading-1" />, div)
-    ReactDOM.unmountComponentAtNode(div)
+  it('heading-1', () => {
+    const rendered = testRenderer.create(
+      <Heading size="heading-1">Hello World</Heading>
+    )
+    expect(rendered.toJSON()).toMatchSnapshot()
+  })
+
+  it('heading-2', () => {
+    const rendered = testRenderer.create(
+      <Heading size="heading-2">Hello World</Heading>
+    )
+    expect(rendered.toJSON()).toMatchSnapshot()
+  })
+
+  it('heading-3', () => {
+    const rendered = testRenderer.create(
+      <Heading size="heading-3">Hello World</Heading>
+    )
+    expect(rendered.toJSON()).toMatchSnapshot()
+  })
+
+  it('heading-4', () => {
+    const rendered = testRenderer.create(
+      <Heading size="heading-4">Hello World</Heading>
+    )
+    expect(rendered.toJSON()).toMatchSnapshot()
   })
 })
