eaab4b970cfd090c34145843e25af6af5f31de8e
Netto Farah <nettofarah@gmail.com>
2020-09-26 10:02:12 -0300

    Document Divider

--- src/components/Divider/Divider.tsx
+++ src/components/Divider/Divider.tsx
@@ -3,10 +3,18 @@ import React, { HTMLAttributes } from 'react'
 
 export const dividerColors = borderColors
 
+/**
+ * Divider Props
+ */
 export interface DividerProps extends HTMLAttributes<HTMLDivElement> {
+  /** The Dracula UI color for the Divider. */
   color: keyof typeof dividerColors
 }
 
+/**
+ * Dividers are horizontal lines used to separate semantic blocks of
+ * content or UI patterns.
+ */
 export const Divider: React.FC<DividerProps> = (props: DividerProps) => {
   const finalProps = {
     ...props,
