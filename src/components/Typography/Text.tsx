7eedbfcc9c2dbec18e006bd188ed19bb9d680b1d
Netto Farah <nettofarah@gmail.com>
2020-09-26 10:42:24 -0300

    Add docs for Text

--- src/components/Typography/Text.tsx
+++ src/components/Typography/Text.tsx
@@ -21,13 +21,41 @@ export const TextColors = mapValues(textColors, (className) => {
   return className.replace('-bg-', '-text-')
 })
 
+/** Text Props */
 export interface TextProps extends HTMLAttributes<HTMLSpanElement> {
+  /**
+   * Controls the size of the text based on pre-configured Dracula UI sizes.
+   * Options: `normal`, `small`, `medium`, `large`
+   */
   size?: keyof typeof TextSizes
+
+  /**
+   * Controsl the weight of the text.
+   * Options: `normal`, `semibold`, `bold`.
+   */
   weight?: keyof typeof TextWeights
+
+  /**
+   * Controls the color of the text
+   */
   color?: keyof typeof TextColors
+
+  /**
+   * Controls the spacing between the Text components and its parent and siblings.
+   */
   spacing?: SpacingPropType
 }
 
+/**
+ * Text is the base component for any sort of text.
+ *
+ * Consumers of this component can control, the color, size,
+ * weight, and spacing of Text.
+ *
+ * Use this component for generic, and non-hierarchical text that is
+ * to be displayed on a page, or as part of other complex components
+ * or UI patterns
+ */
 export const Text = (props: TextProps) => {
   const finalProps = {
     ...props,
@@ -44,6 +72,12 @@ export const Text = (props: TextProps) => {
   return <span {...finalProps}>{props.children}</span>
 }
 
+/**
+ * Paragraph is a semantic component used for blocks of text with
+ * semantic meaning.
+ *
+ * Paragraph accepts all the same customization options as Text.
+ */
 export const Paragraph: React.FC<TextProps> = (props: TextProps) => {
   const finalProps = {
     ...props,
