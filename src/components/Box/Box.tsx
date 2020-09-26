07ae863680ac18ca5fd060648a338b29e746b3e4
Netto Farah <nettofarah@gmail.com>
2020-09-26 09:21:22 -0300

    Document Box

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -3,11 +3,25 @@ import cx from 'classnames'
 import { spacingClasses, SpacingPropType } from '@/base/spacing'
 import { colors } from '@/base/colors'
 
+/**
+ * Box Props
+ */
 export interface BoxProps extends HTMLAttributes<HTMLDivElement> {
+  /** The background color. */
   color?: keyof typeof colors
+
+  /** Dracula UI standard spacing properties. */
   spacing?: SpacingPropType
 }
 
+/**
+ * Box is the most primitive component of Dracula UI.
+ * Using Box allows for consumers of the components library to compose
+ * more complex patterns, components, and UIs.
+ *
+ * Box includes built-in Color and Spacing properties that make building
+ * complex components convenient and consistent.
+ */
 export const Box: React.FC<BoxProps> = (props: BoxProps) => {
   const finalProps = {
     ...props,
