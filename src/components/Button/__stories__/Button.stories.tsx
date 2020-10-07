c620b65dbc0c9ac9dd8ab53a3f875d673020a504
Zeno Rocha <zno.rocha@gmail.com>
2020-10-06 17:41:14 -0700

    Improve <Button /> stories and snippets

--- src/components/Button/__stories__/Button.stories.tsx
+++ src/components/Button/__stories__/Button.stories.tsx
@@ -7,16 +7,12 @@ export default {
 }
 
 export const ButtonNormal = () => {
-  return (
-    <Box style={{ padding: '3rem' }}>
-      <Button color={'purple' as ButtonProps['color']}>normal</Button>
-    </Box>
-  )
+  return <Button color={'purple' as ButtonProps['color']}>Normal</Button>
 }
 
 export const ButtonVariants = () => {
   return (
-    <Box style={{ display: 'flex', flexDirection: 'column', padding: '3rem' }}>
+    <Box>
       {Object.keys(buttonVariants).map((variant) => {
         return (
           <Box key={variant} style={{ marginBottom: 10 }}>
@@ -33,72 +29,63 @@ export const ButtonVariants = () => {
   )
 }
 
-export const ButtonColors = () => (
-  <Box style={{ padding: '1rem 2rem' }}>
-    {Object.keys(colors).map((color) => {
-      return (
-        <Box key={color}>
+export const ButtonDisabled = () => {
+  return (
+    <Box>
+      {Object.keys(buttonVariants).map((variant) => {
+        return (
+          <Box key={variant} style={{ marginBottom: 10 }}>
+            <Button
+              disabled
+              variant={variant as ButtonProps['variant']}
+              color={'purple' as ButtonProps['color']}
+            >
+              {variant}
+            </Button>
+          </Box>
+        )
+      })}
+    </Box>
+  )
+}
+
+export const ButtonColors = () => {
+  return (
+    <Box>
+      {Object.keys(colors).map((color) => {
+        return (
           <Box
+            key={color}
             style={{
               display: 'flex',
               justifyContent: 'space-around'
             }}
           >
-            <Box>
-              <Button
-                color={color as ButtonProps['color']}
-                style={{ margin: '1rem' }}
-              >
-                Normal
-              </Button>
+            <Button
+              color={color as ButtonProps['color']}
+              style={{ margin: '1rem' }}
+            >
+              Normal
+            </Button>
 
-              <Button
-                color={color as ButtonProps['color']}
-                style={{ margin: '1rem' }}
-                disabled
-              >
-                Normal
-              </Button>
-            </Box>
+            <Button
+              color={color as ButtonProps['color']}
+              style={{ margin: '1rem' }}
+              variant="outline"
+            >
+              Outline
+            </Button>
 
-            <Box>
-              <Button
-                color={color as ButtonProps['color']}
-                style={{ margin: '1rem' }}
-                variant="ghost"
-              >
-                Ghost
-              </Button>
-              <Button
-                color={color as ButtonProps['color']}
-                style={{ margin: '1rem' }}
-                variant="ghost"
-                disabled
-              >
-                Ghost
-              </Button>
-            </Box>
-
-            <Box>
-              <Button
-                color={color as ButtonProps['color']}
-                style={{ margin: '1rem' }}
-                variant="outline"
-              >
-                Outline
-              </Button>
-              <Button
-                color={color as ButtonProps['color']}
-                style={{ margin: '1rem' }}
-                variant="outline"
-                disabled
-              >
-                Outline
-              </Button>
-            </Box>
+            <Button
+              color={color as ButtonProps['color']}
+              style={{ margin: '1rem' }}
+              variant="ghost"
+            >
+              Ghost
+            </Button>
           </Box>
-        </Box>
-      )
-    })}
-  </Box>
-)
+        )
+      })}
+    </Box>
+  )
+}
