8d368162fd7848d84fc38ea778c3b00b1a157d23
Netto Farah <nettofarah@gmail.com>
2021-03-09 17:23:24 -0800

    Updates

--- src/story-helpers/random-color.ts
+++
@@ -1,7 +0,0 @@
-import { colors } from '../base/colors'
-
-export const colorname = () => {
-  const colorNames = Object.keys(colors)
-  const index = Math.round(Math.random() * colorNames.length + 1)
-  return colorNames[index]
-}
