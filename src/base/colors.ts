5d1414ac0fc97413cf6456fc67319a42ac2ce8f1
Netto Farah <nettofarah@gmail.com>
2021-03-14 15:06:44 -0700

    Add black colors

--- src/base/colors.ts
+++ src/base/colors.ts
@@ -2,6 +2,9 @@ import { mapValues } from 'lodash'
 
 export const colors = {
   white: 'drac-bg-white',
+  black: 'drac-bg-black',
+  blackSecondary: 'drac-bg-black-secondary',
+  blackLight: 'drac-bg-black-light',
   cyan: 'drac-bg-cyan',
   green: 'drac-bg-green',
   orange: 'drac-bg-orange',
