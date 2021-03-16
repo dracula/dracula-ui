c1f5ddf55484d0a6a7f6d6e05bdadc600d3d4c45
nettofarah <nettofarah@gmail.com>
2021-03-16 16:28:00 -0700

    Update Avatar docs

--- src/components/Avatar/__tests__/Avatar.test.tsx
+++ src/components/Avatar/__tests__/Avatar.test.tsx
@@ -6,23 +6,28 @@ import { withEntries } from '../../../documentation/with-entries'
 
 docs(Avatar, {
   basic: () =>
-    snapshot('basic usage', () => (
-      <Avatar
-        title="Netto Farah"
-        src="https://pbs.twimg.com/profile_images/1217874327137185794/rFqSV_h0_400x400.jpg"
-      />
-    )),
+    snapshot(
+      'basic usage',
+      () => (
+        <Avatar
+          title="Netto Farah"
+          src="https://pbs.twimg.com/profile_images/1217874327137185794/rFqSV_h0_400x400.jpg"
+        />
+      ),
+      `
+      The most basic instance of an Avatar requires a src image and a title to be used for accessibility or as a fallback in case the image can't be loaded.
+    `
+    ),
   variations() {
     const snap = snapshot(
       'Custom colors',
       () => (
         <div>
-          <Avatar title="Netto Farah" color="black" />
           <Avatar title="Netto Farah" color="cyan" />
         </div>
       ),
       `
-    Avatars accept colors, like in the example below...
+      The Avatar component accepts a *color* param that allows for customizing the background and accent color for an Avatar.
     `
     )
 
@@ -30,19 +35,23 @@ docs(Avatar, {
       'Custom variants',
       () => (
         <div>
-          <Avatar title="Netto Farah" variant="normal" />
           <Avatar title="Zeno Rocha" variant="subtle" />
+          <Avatar title="Netto Farah" variant="normal" />
         </div>
       ),
       `
-    Avatars accept multiple variants, like in the example below...
+    Avatars accept multiple variants, which can be used to render a more subtle variation of the Avatar component.
     `
     )
 
     const borders = withEntries(AvatarBorderVariants, (variant) => {
-      return snapshot(`border: ${variant}`, () => (
-        <Avatar title="Netto Farah" borderVariant={variant} />
-      ))
+      return snapshot(
+        `border: ${variant}`,
+        () => <Avatar title="Netto Farah" borderVariant={variant} />,
+        `
+        A border variant allows for customization of the border width of an Avatar, or for an Avatar to be rendered without a border.
+      `
+      )
     })
 
     return [...borders, variants, snap]
