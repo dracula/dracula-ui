1b446adfcecef502c3e45eef2c251f02b003aead
nettofarah <nettofarah@gmail.com>
2021-03-16 19:29:37 -0700

    Document Checkbox

--- src/components/Checkbox/__tests__/Checkbox.test.tsx
+++ src/components/Checkbox/__tests__/Checkbox.test.tsx
@@ -1,20 +1,31 @@
 import { Checkbox } from '@/components/Checkbox/Checkbox'
-import {
-  CheckboxColors,
-  CheckboxDisabled,
-  CheckboxNormal
-} from '@/components/Checkbox/__stories__/Checkbox.stories'
+import { CheckboxColors } from '@/components/Checkbox/__stories__/Checkbox.stories'
 import { docs } from '@/documentation/site-docs'
+import React from 'react'
 import { snapshot } from '../../../documentation/render-component'
 
 docs(Checkbox, {
   basic() {
-    return snapshot('Usage', CheckboxNormal)
+    return snapshot(
+      'Usage',
+      () => (
+        <div>
+          <Checkbox id="one" defaultChecked color="purple" name="demo" />
+          <label htmlFor="one" className="drac-text drac-text-white">
+            Checked
+          </label>
+        </div>
+      ),
+      'Checkbox allows for styling HTML checkboxes with Dracula UI styles.'
+    )
   },
   variations() {
     return [
-      snapshot('Disabled', CheckboxDisabled),
-      snapshot('Colors', CheckboxColors)
+      snapshot(
+        'Colors',
+        CheckboxColors,
+        'Checkboxes can be customized to use any of the Dracula UI theme colors, including all solid, gradient, and animated colors.'
+      )
     ]
   }
 })
