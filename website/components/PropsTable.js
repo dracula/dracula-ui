d47137f4a70dc20b81e41aa770e289fb162b1443
nettofarah <nettofarah@gmail.com>
2021-03-17 09:02:07 -0700

    Remove | undefined

--- website/components/PropsTable.js
+++ website/components/PropsTable.js
@@ -2,31 +2,51 @@ import React from "react"
 import { Box, Paragraph, Text } from "../../dist"
 
 const spacingProps = [
-  'p', 'py', 'px', 'pb', 'pt', 'pl', 'pr',
-  'm', 'my', 'mx', 'mb', 'mt', 'ml', 'mr',
+  "p",
+  "py",
+  "px",
+  "pb",
+  "pt",
+  "pl",
+  "pr",
+  "m",
+  "my",
+  "mx",
+  "mb",
+  "mt",
+  "ml",
+  "mr"
 ]
 
 const PropsTable = ({ props }) => {
   const propList = Object.values(props)
-  const ownProps = propList.filter(p => !spacingProps.includes(p.name))
+  const ownProps = propList.filter((p) => !spacingProps.includes(p.name))
 
   return (
     <>
       {ownProps.map((prop) => (
         <Box my="md">
           <Box>
-            <Text color="purple" weight="semibold">{prop.name}</Text>
-            <Text size="small" color="purple"> {prop.required ? "(required)" : ""}</Text>
+            <Text color="purple" weight="semibold">
+              {prop.name}
+            </Text>
+            <Text size="small" color="purple">
+              {" "}
+              {prop.required ? "(required)" : ""}
+            </Text>
           </Box>
 
           <Box>
-            <Paragraph p="none" size="small">{prop.description}</Paragraph>
+            <Paragraph p="none" size="small">
+              {prop.description}
+            </Paragraph>
           </Box>
 
           <Box>
-            <Text color="green" weight="semibold">values: </Text><br />
             <Text color="green" size="small">
-              {prop.type.name}
+              {prop.type.name
+                .replace("| undefined", "")
+                .replace("undefined", "")}
             </Text>
           </Box>
         </Box>
