b0af6ea8d683a075ce1758d35df06e23991e70d9
nettofarah <nettofarah@gmail.com>
2021-03-17 16:17:15 -0700

    MVP accent color picker

---
+++ website/components/AccentColorPicker.js
@@ -0,0 +1,41 @@
+import { Box, baseColors } from "../../dist"
+
+export function AccentColorPicker() {
+  return (
+    <Box>
+      <ul
+        style={{
+          listStyle: "none",
+          display: "flex",
+          margin: 0,
+          padding: 0
+        }}
+      >
+        {Object.keys(baseColors).map((color) => {
+          return (
+            <li key={color}>
+              <Box
+                onClick={() => {
+                  window.document.documentElement.style.setProperty(
+                    "--accentColor",
+                    window
+                      .getComputedStyle(window.document.documentElement)
+                      .getPropertyValue(`--${color}`)
+                  )
+                }}
+                style={{
+                  width: 12,
+                  height: 12,
+                  margin: 2,
+                  borderRadius: "100%",
+                  cursor: "pointer"
+                }}
+                color={color}
+              />
+            </li>
+          )
+        })}
+      </ul>
+    </Box>
+  )
+}
--- website/components/Topbar.js
+++ website/components/Topbar.js
@@ -2,17 +2,31 @@ import { Box, Text } from "../../dist"
 import { Component } from "react"
 import Link from "next/link"
 import styles from "./Topbar.module.css"
+import { AccentColorPicker } from "./AccentColorPicker"
 
 class Topbar extends Component {
   render() {
     return (
-      <Box p="md">
+      <Box
+        p="md"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          // alignItems: "center",
+          marginLeft: 12
+        }}
+      >
         <Link href="/">
           <a className={styles.title}>
             <Text className={styles.text}>Dracula</Text>
-            <Text className={styles.text} color="purpleCyan"> UI</Text>
+            <Text className={styles.text} color="purpleCyan">
+              {" "}
+              UI
+            </Text>
           </a>
         </Link>
+
+        <AccentColorPicker />
       </Box>
     )
   }
