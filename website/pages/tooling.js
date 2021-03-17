118588d08c630d5545a50e3552d1407868e0697e
Zeno Rocha <zno.rocha@gmail.com>
2021-03-16 17:49:49 -0700

    Add tooling documentation

---
+++ website/pages/tooling.js
@@ -0,0 +1,71 @@
+import React from "react"
+import Head from "next/head"
+import Theme from "../layouts/Theme"
+import { Box, Heading, Link, List, Text, Paragraph } from "../../dist"
+import Navigation from "../components/Navigation"
+import styles from "./index.module.css"
+
+export async function getStaticProps() {
+  return {
+    props: {
+      query: {
+        title: 'Tooling',
+        description: 'How to improve the developer experience in Visual Studio Code'
+      }
+    }
+  }
+}
+
+class Tooling extends React.Component {
+  render() {
+    const { title, description } = this.props.query;
+
+    return (
+      <div>
+        <Head>
+          <meta charSet="utf-8" />
+          <title>{title}</title>
+          <meta content={title} property="og:title" />
+          <meta content={description} name="description" />
+          <meta content={description} property="og:description" />
+          <meta content="Netto Farah &amp; Zeno Rocha" name="author" />
+        </Head>
+
+        <Box className={styles.container}>
+          <Navigation selected={title} />
+          <Box className={styles.content} py="lg">
+            <main className={styles.center}>
+              <Box>
+                <Heading size="heading-1">{title}</Heading>
+                <Paragraph className={styles.description} size="large">{description}</Paragraph>
+                <Box my="lg">
+                  <Heading size="heading-2">Built-in Documentation</Heading>
+                  <Paragraph>We partnered with <Link color="purple" hoverColor="purple" href="https://letsxd.com/vscode">Adobe</Link> to create a new way of reading and consuming documentation without ever leaving the code editor. This new format is called <Link color="purple" hoverColor="purple" href="https://github.com/AdobeXD/design-system-package-dsp">Design System Package (DSP)</Link> and it's open source. Here's a preview:</Paragraph>
+                  <img className={styles.image} src="/static/images/tooling-1.png" alt="Dracula UI documentation on VSCode" />
+                  <Paragraph>If you want to see the Dracula UI documentation inside Visual Studio Code, follow these steps:</Paragraph>
+                  <List color="white" variant="ordered">
+                    <li className="drac-text drac-text-white">Install the <Link color="purple" hoverColor="purple" href="https://marketplace.visualstudio.com/items?itemName=Adobe.xd">Adobe XD extension</Link> on VSCode</li>
+                    <li className="drac-text drac-text-white">Click on the "XD" button on the bottom right corner</li>
+                    <li className="drac-text drac-text-white">Click on the "Load Package" option</li>
+                    <li className="drac-text drac-text-white">Select the <code>dracula-ui/dsp</code> folder</li>
+                    <li className="drac-text drac-text-white">Now you should see the documentation</li>
+                  </List>
+                </Box>
+                <Box my="lg">
+                  <Heading size="heading-2">Autocomplete</Heading>
+                  <Paragraph>In order to have autocomplete enabled on Visual Studio Code you must follow the steps above.</Paragraph>
+                  <Paragraph>Once everything is installed, you should see snippets for HTML and JavaScript code. Here's an example with React:</Paragraph>
+                  <img className={styles.image} src="/static/images/tooling-2.png" alt="Dracula UI autocomplete on VSCode" />
+                </Box>
+              </Box>
+            </main>
+          </Box>
+        </Box>
+      </div>
+    )
+  }
+}
+
+Tooling.Layout = Theme
+
+export default Tooling
