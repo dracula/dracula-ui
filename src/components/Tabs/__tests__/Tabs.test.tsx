d5053aa51b1da01eb9b74550b9ba20e976cca260
Zeno Rocha <zno.rocha@gmail.com>
2021-03-17 18:13:29 -0700

    Document Tabs

--- src/components/Tabs/__tests__/Tabs.test.tsx
+++ src/components/Tabs/__tests__/Tabs.test.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { Tabs } from '@/components/Tabs/Tabs'
 import {
   TabsColors,
@@ -5,8 +6,76 @@ import {
 } from '@/components/Tabs/__stories__/Tabs.stories'
 import { docs } from '@/documentation/site-docs'
 import { snapshot } from '../../../documentation/render-component'
+import { Box } from '../../Box/Box'
 
 docs(Tabs, {
-  basic: () => snapshot('Usage', TabsNormal),
-  variations: () => snapshot('Colors', TabsColors)
+  basic: () => snapshot(
+    'Usage',
+    () => (
+      <Tabs color="white">
+        <li className="drac-tab drac-tab-active">
+          <a className="drac-tab-link drac-text" href="#">
+            Item One
+          </a>
+        </li>
+        <li className="drac-tab">
+          <a className="drac-tab-link drac-text" href="#">
+            Item Two
+          </a>
+        </li>
+        <li className="drac-tab">
+          <a className="drac-tab-link drac-text" href="#">
+            Item Three
+          </a>
+        </li>
+      </Tabs>
+    ),
+    'Display different content based on the Tabs behavior.'
+  ),
+  variations: () => snapshot(
+    'Colors',
+    () => (
+      <Box>
+        <Box>
+          <Tabs color="cyan">
+            <li className="drac-tab drac-tab-active">
+              <a className="drac-tab-link drac-text" href="#">
+                Item One
+              </a>
+            </li>
+            <li className="drac-tab">
+              <a className="drac-tab-link drac-text" href="#">
+                Item Two
+              </a>
+            </li>
+            <li className="drac-tab">
+              <a className="drac-tab-link drac-text" href="#">
+                Item Three
+              </a>
+            </li>
+          </Tabs>
+        </Box>
+        <Box>
+          <Tabs color="green">
+            <li className="drac-tab drac-tab-active">
+              <a className="drac-tab-link drac-text" href="#">
+                Item One
+              </a>
+            </li>
+            <li className="drac-tab">
+              <a className="drac-tab-link drac-text" href="#">
+                Item Two
+              </a>
+            </li>
+            <li className="drac-tab">
+              <a className="drac-tab-link drac-text" href="#">
+                Item Three
+              </a>
+            </li>
+          </Tabs>
+        </Box>
+      </Box>
+    ),
+    'Tabs can be customized to use any of the Dracula UI theme colors.'
+  )
 })
