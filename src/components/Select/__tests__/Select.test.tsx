ebb92159626a8bc598f080281e618e5e580d9e9a
Zeno Rocha <zno.rocha@gmail.com>
2021-03-17 19:32:48 -0700

    Document Select

--- src/components/Select/__tests__/Select.test.tsx
+++ src/components/Select/__tests__/Select.test.tsx
@@ -11,14 +11,34 @@ import { snapshot } from '../../../documentation/render-component'
 
 docs(Select, {
   basic() {
-    return snapshot('Usage', SelectBasic)
+    return snapshot(
+      'Usage',
+      SelectBasic,
+      'Select different items from a list.'
+    )
   },
   variations() {
     return [
-      snapshot('Sizes', SelectSizes),
-      snapshot('Disabled', SelectDisabled),
-      snapshot('Colors', SelectColors),
-      snapshot('Variants', SelectVariants)
+      snapshot(
+        'Sizes',
+        SelectSizes,
+        'Select can be customize to use several different sizes.'
+      ),
+      snapshot(
+        'Variants',
+        SelectVariants,
+        'There are two different variations you can use: `normal` and `outline`'
+      ),
+      snapshot(
+        'Disabled',
+        SelectDisabled,
+        'A disabled select is unusable and un-clickable.'
+      ),
+      snapshot(
+        'Colors',
+        SelectColors,
+        'Select can be customized to use any of the Dracula UI theme colors.'
+      ),
     ]
   }
 })
