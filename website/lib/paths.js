156fe6c8edd0ea60737c186b178b3fd47e9bc2be
nettofarah <nettofarah@gmail.com>
2021-03-31 19:59:48 -0500

    Remove non-unique paths

--- website/lib/paths.js
+++ website/lib/paths.js
@@ -1,23 +1,25 @@
 import dspComponents from "@dracula/dracula-ui/dsp/data/components.json"
 
-const paths = dspComponents.entities.map((entity) => {
-  return {
-    params: {
-      guide: entity.name.toLowerCase(),
-      title: entity.name,
-      description: entity.description,
-      docgen: entity.ext_com_draculaui_docgen,
-      sections: Object.values(entity.ext_com_draculaui_variations).map(
-        (example) => {
-          return {
-            ...example,
-            code: example.html,
-            description: example.docs
+const paths = dspComponents.entities
+  .filter((entity) => !["Spacing", "Colors"].includes(entity.name))
+  .map((entity) => {
+    return {
+      params: {
+        guide: entity.name.toLowerCase(),
+        title: entity.name,
+        description: entity.description,
+        docgen: entity.ext_com_draculaui_docgen,
+        sections: Object.values(entity.ext_com_draculaui_variations).map(
+          (example) => {
+            return {
+              ...example,
+              code: example.html,
+              description: example.docs
+            }
           }
-        }
-      )
+        )
+      }
     }
-  }
-})
+  })
 
 module.exports = paths
