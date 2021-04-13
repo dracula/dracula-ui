eae52f9f618e38a9962c36c7ea79cb23f68a460d
vitormalencar <vitor.maia.alencar@gmail.com>
2021-04-13 20:20:49 +0200

    Adding Copy to clipboard button

--- website/components/CodeHighlight.js
+++ website/components/CodeHighlight.js
@@ -1,10 +1,27 @@
-import { Component } from "react"
+import React, { Component } from "react"
 import localStyles from "./CodeHighlight.module.css"
 import Highlight, { defaultProps } from "prism-react-renderer"
 import theme from "../lib/prism"
+import ClipBoard from "clipboard"
+
+import { Button } from "@dracula/dracula-ui"
 
 class CodeHighlight extends Component {
+  state = {
+    visibility: "hidden"
+  }
+
+  copyButton = React.createRef()
+
+  componentDidMount() {
+
+      const clipboard = new ClipBoard(this.copyButton.current, {
+        text: (trigger) => this.props.code
+      })
+  }
+
   render() {
+    const visibility = this.state.visibility
     return (
       <Highlight
         {...defaultProps}
@@ -13,15 +30,33 @@ class CodeHighlight extends Component {
         code={this.props.code}
       >
         {({ style, tokens, getLineProps, getTokenProps }) => (
-          <pre className={localStyles.pre} style={style}>
-            {tokens.map((line, i) => (
-              <div {...getLineProps({ line, key: i })}>
-                {line.map((token, key) => (
-                  <span {...getTokenProps({ token, key })} />
-                ))}
-              </div>
-            ))}
-          </pre>
+          <div
+            onMouseEnter={() => this.setState({ visibility: "visible" })}
+            onMouseLeave={() => this.setState({ visibility: "hidden" })}
+          >
+
+            <pre className={localStyles.pre} style={style}>
+             <span className={localStyles.copyButtonContainer}>
+              <Button
+                m="sm"
+                size="sm"
+                ref={this.copyButton}
+                style={{ visibility }}
+                aria-label="Copy code to clipboard"
+                data-clipboard-text={this.props.code}
+              >
+                Copy
+              </Button>
+            </span>
+              {tokens.map((line, i) => (
+                <div {...getLineProps({ line, key: i })}>
+                  {line.map((token, key) => (
+                    <span {...getTokenProps({ token, key })} />
+                  ))}
+                </div>
+              ))}
+            </pre>
+          </div>
         )}
       </Highlight>
     )
