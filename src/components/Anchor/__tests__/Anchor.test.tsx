c7aa56dc8d4dc41a7326c3a0cde270642a562315
Zeno Rocha <zno.rocha@gmail.com>
2021-04-16 04:28:28 -0700

    Break down Size & Weight from Anchor demo

--- src/components/Anchor/__tests__/Anchor.test.tsx
+++ src/components/Anchor/__tests__/Anchor.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { docs } from '@/documentation/site-docs'
 import { snapshot } from '@/documentation/render-component'
+import { Box } from '@/components/Box/Box'
 import { Anchor } from '@/components/Anchor/Anchor'
 
 docs(Anchor, {
@@ -14,15 +15,20 @@ docs(Anchor, {
   variations() {
     return [
       snapshot(
-        'Sizes & Weights',
-        SizesWeights,
-        `Anchor sizes can be customized with pre defined sizes or using regular HTML styles or CSS classes.`
-      ),
-      snapshot(
         'Colors',
         Colors,
         `Anchors can be customized to use any of the Dracula UI theme colors, including all solid, and gradients.`
-      )
+      ),
+      snapshot(
+        'Weights',
+        Weights,
+        'Anchors can be defined in different font weights.'
+      ),
+      snapshot(
+        'Sizes',
+        Sizes,
+        'Anchors can be customize to use several different sizes.'
+      ),
     ]
   }
 })
@@ -33,39 +39,35 @@ function Usage() {
   )
 }
 
-function SizesWeights() {
+function Weights() {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column' }}>
-      <Anchor href="#" size="xs">
-        xs
-      </Anchor>
-      <Anchor href="#" size="sm">
-        sm
-      </Anchor>
-      <Anchor href="#" size="md">
-        md
-      </Anchor>
-      <Anchor href="#" size="lg">
-        lg
-      </Anchor>
-      <Anchor href="#" weight="semibold">
-        semibold
-      </Anchor>
-      <Anchor href="#" style={{ fontSize: 30 }}>
-        font-size: 30
-      </Anchor>
-    </div>
+    <Box style={{ display: 'flex', flexDirection: 'column' }}>
+      <Anchor href="#" weight="normal" mb="sm">normal</Anchor>
+      <Anchor href="#" weight="semibold" mb="sm">semibold</Anchor>
+      <Anchor href="#" weight="bold" mb="sm">bold</Anchor>
+    </Box>
+  )
+}
+
+function Sizes() {
+  return (
+    <Box style={{ display: 'flex', flexDirection: 'column' }}>
+      <Anchor href="#" size="xs" mb="sm">xs</Anchor>
+      <Anchor href="#" size="sm" mb="sm">sm</Anchor>
+      <Anchor href="#" size="md" mb="sm">md</Anchor>
+      <Anchor href="#" size="lg" mb="sm">lg</Anchor>
+    </Box>
   )
 }
 
 function Colors() {
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
-      <Anchor href="#" color="pink" hoverColor="purple" m="sm">
-        pink / purple
+      <Anchor href="#" color="purple" hoverColor="pink" mb="sm">
+        purple / pink
       </Anchor>
 
-      <Anchor href="#" color="cyanGreen" hoverColor="yellowPink" m="sm">
+      <Anchor href="#" color="cyanGreen" hoverColor="yellowPink" mb="sm">
         cyanGreen / yellowPink
       </Anchor>
     </div>
