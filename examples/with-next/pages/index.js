38b7adfc79b9017487fbdeb10d82b2c433537a8a
Helder Burato Berto <helder.burato@gmail.com>
2021-04-23 18:42:19 +0100

    Update NextJS example (#87)
    It uses the more common approach without the 'extends Component' and
    'import react' because it isn't required in the new versions of React/NextJS.

--- examples/with-next/pages/index.js
+++ examples/with-next/pages/index.js
@@ -1,10 +1,5 @@
-import { Component } from 'react'
 import { Paragraph } from '@dracula/dracula-ui'
 
-class Index extends Component {
-  render() {
-    return <Paragraph color="black">Hello Vampire</Paragraph>
-  }
+export default function Index() {
+  return <Paragraph color="black">Hello Vampire</Paragraph>
 }
-
-export default Index
