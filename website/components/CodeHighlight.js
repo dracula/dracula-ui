fd83c9aaf4eb710546f7e797a76afc503c72dadd
Zeno Rocha <zno.rocha@gmail.com>
2021-07-17 23:08:13 -0700

    Improve copy-to-clipboard interaction

--- website/components/CodeHighlight.js
+++ website/components/CodeHighlight.js
@@ -4,10 +4,8 @@ import Highlight, { defaultProps } from 'prism-react-renderer'
 import theme from '../lib/prism'
 import CopyToClipboard from 'clipboard'
 
-import { Button } from '@dracula/dracula-ui'
-
-class CodeHighlight extends Component {
-  state = { visibility: 'hidden', copyText: 'copy' }
+export default class CodeHighlight extends Component {
+  state = { copied: false }
   copyButton = React.createRef()
 
   componentDidMount() {
@@ -19,16 +17,47 @@ class CodeHighlight extends Component {
   onCopy = () => {
     this.setState(
       {
-        copyText: 'copied!'
+        copied: true
       },
       () => {
-        setTimeout(() => this.setState({ copyText: 'copy' }), 1000)
+        setTimeout(() => this.setState({ copied: false }), 1000)
       }
     )
   }
 
+  renderButton() {
+    const { copied } = this.state
+
+    return (
+      <div className={localStyles.copyButtonContainer}>
+        <button
+          ref={this.copyButton}
+          aria-label="Copy to clipboard"
+          data-clipboard-text={this.props.code}
+          onClick={this.onCopy}
+          className={localStyles.copyButton}
+        >
+          <div className={localStyles.copyIcon}>
+            <Clippy
+              className={localStyles.clippy}
+              style={{
+                strokeDashoffset: copied ? -50 : 0
+              }}
+            />
+            <Check
+              isVisible={copied}
+              className={localStyles.check}
+              style={{
+                strokeDashoffset: copied ? 0 : -50
+              }}
+            />
+          </div>
+        </button>
+      </div>
+    )
+  }
+
   render() {
-    const visibility = this.state.visibility
     return (
       <Highlight
         {...defaultProps}
@@ -37,24 +66,9 @@ class CodeHighlight extends Component {
         code={this.props.code}
       >
         {({ style, tokens, getLineProps, getTokenProps }) => (
-          <div
-            onMouseEnter={() => this.setState({ visibility: 'visible' })}
-            onMouseLeave={() => this.setState({ visibility: 'hidden' })}
-          >
+          <div>
             <div style={{ position: 'relative' }}>
-              <span className={localStyles.copyButtonContainer}>
-                <Button
-                  m="xs"
-                  size="xs"
-                  ref={this.copyButton}
-                  style={{ visibility }}
-                  aria-label="Copy to clipboard"
-                  data-clipboard-text={this.props.code}
-                  onClick={this.onCopy}
-                >
-                  {this.state.copyText}
-                </Button>
-              </span>
+              {this.renderButton()}
               <pre className={localStyles.pre} style={style}>
                 {tokens.map((line, i) => (
                   <div {...getLineProps({ line, key: i })}>
@@ -72,4 +86,39 @@ class CodeHighlight extends Component {
   }
 }
 
-export default CodeHighlight
+function Clippy(props) {
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path d="M5.75 4.75H10.25V1.75H5.75V4.75Z" />
+      <path d="M3.25 2.88379C2.9511 3.05669 2.75 3.37987 2.75 3.75001V13.25C2.75 13.8023 3.19772 14.25 3.75 14.25H12.25C12.8023 14.25 13.25 13.8023 13.25 13.25V3.75001C13.25 3.37987 13.0489 3.05669 12.75 2.88379" />
+    </svg>
+  );
+}
+
+function Check(props) {
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path d="M13.25 4.75L6 12L2.75 8.75" />
+    </svg>
+  );
+}
