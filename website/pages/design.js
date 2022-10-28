c7f881c3b97fef83024bc82d05d5fb1451277cc1
luxonauta <luxon4uta@gmail.com>
2022-10-28 17:21:36 -0300

    📝 Fixes a typo on the Design page

--- website/pages/design.js
+++ website/pages/design.js
@@ -1,14 +1,15 @@
-import React from "react"
-import { Anchor, Box, Heading, Paragraph } from "dracula-ui"
-import styles from "./index.module.css"
-import Docs from "../layouts/Docs"
+import { Anchor, Box, Heading, Paragraph } from 'dracula-ui'
+
+import Docs from '../layouts/Docs'
+import React from 'react'
+import styles from './index.module.css'
 
 export async function getStaticProps() {
   return {
     props: {
       query: {
-        title: "Design",
-        description: "How to use Dracula UI on Figma."
+        title: 'Design',
+        description: 'How to use Dracula UI on Figma.'
       }
     }
   }
@@ -19,14 +20,22 @@ class Design extends React.Component {
     return (
       <Box>
         <Box my="lg">
-          <Heading as="h2" size="xl">Using Figma</Heading>
+          <Heading as="h2" size="xl">
+            Using Figma
+          </Heading>
           <Paragraph>
-            To facilitate the communication between between designers
-            and developers, we decided to build all Dracula UI
-            components on Figma.
+            To facilitate the communication between designers and developers, we
+            decided to build all Dracula UI components on Figma.
           </Paragraph>
           <Paragraph>
-            You can find the <code>.fig</code> file inside the <Anchor target="_blank" href="https://github.com/dracula/dracula-ui/tree/main/design">design</Anchor> folder.
+            You can find the <code>.fig</code> file inside the{' '}
+            <Anchor
+              target="_blank"
+              href="https://github.com/dracula/dracula-ui/tree/main/design"
+            >
+              design
+            </Anchor>{' '}
+            folder.
           </Paragraph>
           <img
             className={styles.image}
